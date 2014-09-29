fe689f7cf6cab68ed0934a44f8abf2d035759c57
maestropandy <maestropandy@gmail.com>
2014-09-29 17:43:25 +0530

    KMS CONFIGURATION IN WIN-2008

---
+++ KMS_WS_2008.bat
@@ -0,0 +1,6 @@
+cscript C:\Windows\System32\slmgr.vbs /upk
+cscript C:\Windows\System32\slmgr.vbs /ipk YC6KT-GKW9T-YTKYR-T4X34-R7VHC
+cscript C:\Windows\System32\slmgr.vbs /skms 10.137.2.19:1688
+cscript C:\Windows\System32\slmgr.vbs /ato
+TZUTIL /s "Indian Standard Time"
+Done.
